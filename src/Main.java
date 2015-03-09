e47a6bd5fccaafc7cbb76e4cdcd9c57baa61bef8
reximir <davidlibraboy@gmail.com>
2015-03-09 10:27:03 +0700

    add git ignore

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * https://n3vrax.wordpress.com/2011/08/14/aesrijndael-java-implementation/
+ * @author David S
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {		
+		try{
+			//AES
+			String plainText;
+			
+			/**From file*/
+			plainText = readFromFile("File/input.txt");
+			/**Hardcode mode*/
+			plainText = "makan nasi";			
+
+			/**Key Algorithm*/
+			String k = "kriptografi";
+			System.out.println("Plain Text: "+plainText);
+			byte[] enc = AES.encrypt(plainText.getBytes(), k.getBytes());
+			System.out.println("Encrypted text AES: "+new String(enc));
+
+			byte[] dec = AES.decrypt(enc, k.getBytes());
+			System.out.println("Decrypted text AES: "+new String(dec));
+			System.out.println("------------------");
+	
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Return string formatted after open a file
+	 * @param path
+	 * @return
+	 */
+	private static String readFromFile(String path) {	
+		String results = "";
+		BufferedReader br = null;
+		 
+		try {
+ 
+			String sCurrentLine;
+ 
+			br = new BufferedReader(new FileReader(path));
+ 
+			while ((sCurrentLine = br.readLine()) != null) {
+				results+=sCurrentLine;
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return results;
+	}
+
+}
